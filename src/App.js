44bf22edd3225e23b017acce11cba7270f3dfdaf
Bhoomi <bhoomigupta140520@gmail.com>
2025-06-13 21:12:17 +0530

    protected all page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './css/App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Home from './components/Home/Home';
-import AboutUsContent from "./components/Aboutus/Aboutuscontent"; // Adjust the path if necessary
+import AboutUsContent from "./components/Aboutus/Aboutuscontent";
 import Help from './components/Pages/Help';
 import Reviewcontent from './components/Review/Reviewcontent';
 import Login from './components/log/Login';
 import Signup from './components/log/Signup';
 import Profile from './components/Profile/Profile';
 import ForgotPassword from './components/log/ForgotPassword';
-import Main from './components/Home/Main'; 
+import Main from './components/Home/Main';
 import ProtectedRoute from './components/ProtectedRoutes';
 
 function App() {
@@ -17,9 +17,9 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-        
-         {/*  Protected Home route */}
-         <Route
+
+          {/* Protected Home route */}
+          <Route
             path="/"
             element={
               <ProtectedRoute>
@@ -28,31 +28,62 @@ function App() {
             }
           />
 
-          {/* About Us route */}
-          <Route path="/aboutus" element={<AboutUsContent />} />
+          {/* Protected About Us route */}
+          <Route
+            path="/aboutus"
+            element={
+              <ProtectedRoute>
+                <AboutUsContent />
+              </ProtectedRoute>
+            }
+          />
 
-          {/* Help route */}
-          <Route path="/help" element={<Help />} />
+          {/* Protected Help route */}
+          <Route
+            path="/help"
+            element={
+              <ProtectedRoute>
+                <Help />
+              </ProtectedRoute>
+            }
+          />
 
-          {/* review route */}
-          <Route path="/review" element={<Reviewcontent />} />
+          {/* Protected Review route */}
+          <Route
+            path="/review"
+            element={
+              <ProtectedRoute>
+                <Reviewcontent />
+              </ProtectedRoute>
+            }
+          />
 
-          {/* Login route */}
+          {/* Protected Profile route */}
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Protected Main route */}
+          <Route
+            path="/main"
+            element={
+              <ProtectedRoute>
+                <Main />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Public Routes */}
           <Route path="/login" element={<Login />} />
-
-          {/* Signup route */}
           <Route path="/signup" element={<Signup />} />
-
-          {/* Forgot Password route */}
           <Route path="/ForgotPassword" element={<ForgotPassword />} />
 
-          {/* Profile route */}
-          <Route path="/profile" element={<Profile />} />
-
-          {/* Main route */}
-          <Route path="/main" element={<Main />} /> {/* Link for "Start" */}
-
-          {/* Redirect unknown routes to home */}
+          {/* Redirect unknown routes */}
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
